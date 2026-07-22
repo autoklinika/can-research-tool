28ab0695046b28074b0c1cbd5bcab8814713e867
autoklinika <86266304+autoklinika@users.noreply.github.com>
2026-07-22 13:40:45 +0200

    Build project properties menu without reusing deleted Qt wrappers

--- gui/project_properties_shell.py
+++ gui/project_properties_shell.py
@@ -23,21 +23,41 @@ class ProjectPropertiesMainWindow(SearchEnabledMainWindow):
         self.project_properties_action.triggered.connect(self._edit_project)
 
     def _build_menu(self) -> None:
-        super()._build_menu()
-        file_menu = next(
-            (
-                action.menu()
-                for action in self.menuBar().actions()
-                if action.menu() is not None
-                and action.text().replace("&", "") == "Plik"
-            ),
-            None,
-        )
-        if file_menu is None:
-            return
-        actions = file_menu.actions()
-        before = actions[2] if len(actions) >= 3 else None
-        file_menu.insertAction(before, self.project_properties_action)
+        menu_bar = self.menuBar()
+        menu_bar.clear()
+
+        file_menu = menu_bar.addMenu("Plik")
+        file_menu.addAction(self.new_project_action)
+        file_menu.addAction(self.open_project_action)
+        file_menu.addAction(self.project_properties_action)
+        file_menu.addSeparator()
+        file_menu.addAction(self.import_action)
+        file_menu.addAction(self.save_log_action)
+        file_menu.addSeparator()
+        file_menu.addAction(self.exit_action)
+
+        view_menu = menu_bar.addMenu("Widok")
+        view_menu.addAction(self.toggle_explorer_action)
+        view_menu.addAction(self.toggle_inspector_action)
+        view_menu.addAction(self.toggle_output_action)
+        view_menu.addSeparator()
+        view_menu.addAction(self.toggle_primary_toolbar_action)
+        view_menu.addAction(self.reset_layout_action)
+
+        capture_menu = menu_bar.addMenu("Capture")
+        capture_menu.addAction(self.live_action)
+
+        analysis_menu = menu_bar.addMenu("Analiza")
+        analysis_menu.addAction(self.search_action)
+        analysis_menu.addAction(self.compare_action)
+        analysis_menu.addAction(self.signals_action)
+
+        tools_menu = menu_bar.addMenu("Narzędzia")
+        tools_menu.addAction(self.decoders_action)
+        tools_menu.addAction(self.filters_action)
+        tools_menu.addAction(self.session_markers_action)
+        tools_menu.addSeparator()
+        tools_menu.addAction(self.settings_action)
 
     def _set_project(self, project) -> None:
         super()._set_project(project)
